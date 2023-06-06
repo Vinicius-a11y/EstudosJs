a951bb442317ef5c9eb14f7ab9938acb28fbab6d
Vinicius Bueno <79877937+Vinicius-a11y@users.noreply.github.com>
2023-06-06 09:43:06 -0300

    Exercicios

---
+++ Estudos/js/sipnner.js
@@ -0,0 +1,9 @@
+console.log('Vinicius Bueno');       // Strinh = Texto
+console.log("Vinicius Bueno");
+console.log(`'Vinicius' "Bueno"`);
+
+console.log(35, 15.85, 'Vinicius Bueno');
+
+console.log('Meu nome é Vincius. Estou aprendendo JavaScript as' , 10, 'da manha.');
+
+console.log('Este é uma exercicios de JavaScript pois hoje é dia', 06/06/2023, 'E tenho 21 anos nesse momento');
